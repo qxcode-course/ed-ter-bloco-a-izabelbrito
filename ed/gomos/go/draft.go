d1136e6467a89950c5edb1c272143cbc93de9041
Izabel Brito <izabelbritomelo1@gmail.com>
2026-03-22 23:14:23 -0300

    questoes finalizadas

--- ed/gomos/go/draft.go
+++ ed/gomos/go/draft.go
@@ -1,5 +1,38 @@
 package main
+
 import "fmt"
-func main() {
-    fmt.Println("qxcode")
+
+func main(){
+
+    var q int
+    var d string
+
+    fmt.Scan(&q, &d)
+
+    x := make([]int, q)
+	y := make([]int, q)
+
+    for i := 0; i < q; i++{
+		fmt.Scan(&x[i], &y[i])
+	}
+
+    for i := q -1; i > 0; i--{
+        x[i] = x[i - 1]
+        y[i] = y[i - 1]    
+    }
+
+    if d == "L"{
+		x[0]--
+	}else if d == "R"{
+		x[0]++
+	}else if d == "U"{
+		y[0]--
+	}else if d == "D"{
+		y[0]++
+	}
+
+    for i := 0; i < q; i++{
+        fmt.Println(x[i], y[i])
+    }
+   
 }
