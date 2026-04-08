3d48be3d32e412d51ce112518384b1ca16571030
Izabel Brito <izabelbritomelo1@gmail.com>
2026-04-07 22:58:14 -0300

    questao triangulo

--- ed/triangulo/src/go/main.go
+++ ed/triangulo/src/go/main.go
@@ -9,11 +9,21 @@ import (
 )
 
 func processa(vet []int) {
-	_ = vet;
-	// 1. defina o ponto de parada
-	// 2. monte o vetor auxiliar com os resultados das somas
-	// 3. chame recursivamente a função processa para o vetor auxiliar
-	// 4. imprima o vetor original
+
+	if len(vet) == 0{
+		return
+	}
+
+	aux := []int{}
+
+	for i := 0; i < len(vet)-1; i++{
+        aux = append(aux, vet[i] + vet[i+1])
+    }
+
+	processa(aux)
+
+	fmt.Printf("[ %s ]\n", Join(vet, " "))
+	
 }
 
 func main() {
