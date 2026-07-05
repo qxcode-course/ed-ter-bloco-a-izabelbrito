bd61ccd7a53a0e9996e05aac302fec61f192d208
Izabel Brito <izabelbritomelo1@gmail.com>
2026-07-05 15:07:31 -0300

    atualizando

---
+++ ed/caminho/src/go/main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Pos struct {
+	l, c int
+}
+
+func (p Pos) getNeig() []Pos {
+	return []Pos{
+		{p.l - 1, p.c},
+		{p.l + 1, p.c},
+		{p.l, p.c - 1},
+		{p.l, p.c + 1},
+	}
+}
+
+func inside(grid [][]rune, pos Pos) bool {
+	nrows := len(grid)
+	ncols := len(grid[0])
+	return pos.l >= 0 && pos.l < nrows && pos.c >= 0 && pos.c < ncols
+}
+
+func match(grid [][]rune, pos Pos, char rune) bool {
+	return inside(grid, pos) && grid[pos.l][pos.c] == char
+}
+
+func search(grid [][]rune, startPos Pos, endPos Pos) {
+	fila := NewQueue[Pos]()
+	visitados := make(map[Pos]bool)
+	cameFrom := make(map[Pos]Pos)
+
+	fila.Enqueue(startPos)
+	visitados[startPos] = true
+	
+
+	for !fila.IsEmpty(){
+		atual, _ := fila.Dequeue()
+
+		if atual == endPos{
+			break
+		}
+
+		for _, vizinho := range atual.getNeig(){
+			if match(grid, vizinho, ' ') && !visitados[vizinho]{
+				visitados[vizinho] = true
+
+				cameFrom[vizinho] = atual
+				fila.Enqueue(vizinho)
+			}
+		}
+	}
+
+	voltar(grid, cameFrom, startPos, endPos)
+}
+
+func voltar(grid [][]rune, cameFrom map[Pos]Pos, startPos Pos, endPos Pos){
+	_, achou := cameFrom[endPos]
+	if !achou{
+		return
+	}
+	atual := endPos
+
+	for atual != startPos{
+		grid[atual.l][atual.c] = '.'
+		atual = cameFrom[atual]
+	}
+
+	grid[startPos.l][startPos.c] = '.'
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var nl, nc int
+	scanner.Scan()
+	line := scanner.Text()
+	fmt.Sscanf(line, "%d %d", &nl, &nc)
+	mat := make([][]rune, nl) // Inicializa a matriz de runes
+
+	// Carregando matriz
+	for i := range nl {
+		scanner.Scan()
+		line := scanner.Text()
+		mat[i] = []rune(line)
+	}
+
+	var inicio, fim Pos
+
+	// Procurando inicio e fim e colocando ' ' nas posições iniciais
+	for l := range nl {
+		for c := range nc {
+			if mat[l][c] == 'I' {
+				mat[l][c] = ' '
+				inicio = Pos{l, c}
+			}
+			if mat[l][c] == 'F' {
+				mat[l][c] = ' '
+				fim = Pos{l, c}
+			}
+		}
+	}
+
+	search(mat, inicio, fim)
+
+	for _, line := range mat {
+		fmt.Println(string(line)) // Converte o slice de runes de volta para string para imprimir
+	}
+}
